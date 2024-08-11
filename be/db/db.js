f52e6dca19a27650b8c60cfa05e93fa981b867a8
rina <61769346+numerikid@users.noreply.github.com>
2024-08-11 13:29:44 +0700

    update

---
+++ be/db/db.js
@@ -0,0 +1,18 @@
+// db/db.js
+const mysql = require('mysql2');
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'kopi_skripsi'
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+    } else {
+        console.log('Connected to MySQL database.');
+    }
+});
+
+module.exports = db;
